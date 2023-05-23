c088f061ff71e3f88732b3bcc1f9100b7989245e
sablevsky <vladislav1145@gmail.com>
2023-05-23 18:09:45 +0300

    Add contants export

--- core/index.ts
+++ core/index.ts
@@ -2,12 +2,14 @@ import accounts from './accounts'
 import common from './common'
 import nfts from './nfts'
 import other from './other'
+import * as constants from './constants'
 
-export { accounts, common, nfts, other }
+export { accounts, common, nfts, other, constants }
 
 export default {
   accounts,
   common,
   nfts,
   other,
+  constants,
 }
